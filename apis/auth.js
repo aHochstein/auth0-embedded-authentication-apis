1538f262ea0fcbba4987d46c7354a88e8c61371d
André Hochstein <andr.hochstein@gmail.com>
2023-08-25 10:01:25 +0200

    First draft MFA APi

--- apis/auth.js
+++ apis/auth.js
@@ -2,11 +2,14 @@ const config = require('../config.json');
 const axios = require('axios');
 const querystring = require('querystring');
 const baseUrl = `https://${config.tenantDomain}/`;
+const mfaAudience = `https://${config.tenantDomain}/mfa/`;
 const connection = config.connection;
 const clientId = config.clientId;
 const clientSecret = config.clientSecret;
 const signUpUrl = `${baseUrl}dbconnections/signup`;
 const loginUrl = `${baseUrl}oauth/token`;
+const getAuthenticatorsUrl = `${baseUrl}mfa/authenticators`;
+const requestChallengeUrl = `${baseUrl}mfa/challenge`;
 
 
 
@@ -35,10 +38,57 @@ const login = async(email,password) => {
             client_secret : clientSecret,
             username : email,
             password : password,
-            realm: connection
+            realm: connection,
+            audience: mfaAudience
         }
         const response = await axios.post(loginUrl, body,{ 
             headers: {'content-type': 'application/x-www-form-urlencoded'}});
+        return response.data;        
+    }
+    catch(e) {
+        console.log(e);
+    }
+}
+
+const verifyWithOtp = async(otp,mfa_token) => {
+    try {
+        const body =  {
+            grant_type: 'http://auth0.com/oauth/grant-type/mfa-otp',
+            client_id : clientId,
+            client_secret : clientSecret,
+            mfa_token : mfa_token,
+            otp : otp
+        }
+        const response = await axios.post(loginUrl, body,{ 
+            headers: {'content-type': 'application/x-www-form-urlencoded'}});
+        return response.data;        
+    }
+    catch(e) {
+        console.log(e);
+    }
+}
+
+const requestChallenge = async(mfa_token) => {
+    try {
+        const body =  {
+            client_id : clientId,
+            client_secret : clientSecret,
+            mfa_token : mfa_token,
+            otp : otp
+        }
+        const response = await axios.post(requestChallengeUrl, body);
+        return response.data;        
+    }
+    catch(e) {
+        console.log(e);
+    }
+}
+
+
+const getAuthenticators = async(accessToken) => {
+    try {
+        const response = await axios.get(getAuthenticatorsUrl, 
+            {headers: {'Authorization': `Bearer ${accessToken}`}});
         return response.data;        
     }
     catch(e) {
